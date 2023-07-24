85fc413f044288d0f29130aed825e03449b29570
Ryan J Schave <rjschave@gmail.com>
2023-07-24 11:41:56 -0400

    Removes nullable option;

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Demo(models.Model):
     name = models.CharField(max_length=50)
-    another_field = models.IntegerField(null=True)
+    another_field = models.IntegerField()
 
     class Meta:
         indexes = [
