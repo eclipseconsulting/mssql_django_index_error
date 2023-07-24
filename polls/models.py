4eb8961c93a578b19cc62e739c756ab2301b5159
Ryan J Schave <rjschave@gmail.com>
2023-07-24 11:40:52 -0400

    Adds nullable column with index in Meta class;

--- polls/models.py
+++ polls/models.py
@@ -3,5 +3,9 @@ from django.db import models
 
 class Demo(models.Model):
     name = models.CharField(max_length=50)
+    another_field = models.IntegerField(null=True)
 
-
+    class Meta:
+        indexes = [
+            models.Index(fields=['name', 'another_field'], name='my_custom_index')
+        ]
